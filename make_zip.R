48f290ae69feb325eb7015af518a2a01e3bfae6d
markdunning <mark.dunning@gmail.com>
2017-03-28 13:29:35 +0100

    Create more-realistic names for patients in dataset and re-compile

--- make_zip.R
+++ make_zip.R
@@ -2,4 +2,5 @@ files <- list.files(pattern="*.Rmd")
 files <- c(files, "patient-data.txt","patient-data-cleaned.txt","clinicalData.txt","tidyr-example.txt","diabetes.txt")
 files <- c(files, "images/")
 files <- files[-grep("solutions",files)]
+files <- files[-grep("images.Rmd",files)]
 zip(files, zipfile = "Course_Data.zip")
