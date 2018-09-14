3f370676a51cf20da96a1682678136c5afa83247
eldrid01 <matthew.d.eldridge@gmail.com>
2018-09-14 15:33:05 +0100

    Updated to use an explicit list of files to include in the course data zip file.

--- make_zip.R
+++ make_zip.R
@@ -1,6 +1,20 @@
-files <- list.files(pattern="*.Rmd")
-files <- c(files, "patient-data.txt","patient-data-cleaned.txt","clinicalData.txt","tidyr-example.txt","diabetes.txt")
-files <- c(files, "images/")
-files <- files[-grep("solutions",files)]
-files <- files[-grep("images.Rmd",files)]
+files <- c(
+	"1.introduction.html",
+	"2.dplyr-intro.nb.html",
+	"3.workflows.nb.html",
+	"4.summarise-and-combine.nb.html",
+	"ggplot2.html",
+	"2.dplyr-intro-exercises.Rmd",
+	"3.workflows-exercises.Rmd",
+	"4.summarise-and-combine-exercises.Rmd",
+	"ggplot2-exercises1-images.html",
+	"ggplot2-exercises1.Rmd",
+	"ggplot2-exercises2-images.html",
+	"ggplot2-exercises2.Rmd",
+	"patient-data.txt",
+	"patient-data-cleaned.txt",
+	"clinicalData.txt",
+	"tidyr-example.txt",
+	"diabetes.txt"
+)
 zip(files, zipfile = "Course_Data.zip")
